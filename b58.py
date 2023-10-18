e4d315ee305bed9147daaecf4afeaab123a263fc
Xuan Chen <87976327+xuanchenunimelb@users.noreply.github.com>
2023-10-18 17:30:47 +1100

    Finalize code

--- b58.py
+++
@@ -1,8 +0,0 @@
-import base58
-
-text = "['val0', 'val1']"
-base58Str = base58.b58encode(text.encode('utf-8')).decode('utf-8')
-print(base58Str)
-
-text = base58.b58decode(base58Str).decode('utf-8')
-print(text)
